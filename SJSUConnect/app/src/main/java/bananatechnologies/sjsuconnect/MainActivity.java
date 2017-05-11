a96b2b15da284939ce7595735e869f8da12cbb97
Sagar Mane <sagar.mane006@gmail.com>
2017-05-11 11:25:02 -0700

    Starting the login module

--- SJSUConnect/app/src/main/java/bananatechnologies/sjsuconnect/MainActivity.java
+++ SJSUConnect/app/src/main/java/bananatechnologies/sjsuconnect/MainActivity.java
@@ -2,12 +2,40 @@ package bananatechnologies.sjsuconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private static final String TAG = "logs";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email=(EditText) findViewById(R.id.email);
+        password=(EditText) findViewById(R.id.password);
+        Log.i(TAG,"Reporting from onCreate view");
+        login=(Button) findViewById(R.id.log_in);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"Reporting from login button pressed");
+                Log.i(TAG,"Email="+email.getText()+"Password="+password.getText());
+
+                if(email.getText().equals("admin")&&password.getText().equals("admin")){
+                    Log.i(TAG,"Successful login");
+                }
+                else{
+                    Log.i(TAG,"Login failed");
+                }
+            }
+        });
     }
+
 }
