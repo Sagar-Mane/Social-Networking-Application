94ddf328be51aa88fad2e39cf76205e4a85ec1fa
jnirgudkar1 <jnirgudkar1@gmail.com>
2017-05-12 16:12:30 -0700

    MongoDB connections

--- src/SJSU-Connect-Server/routes/user.js
+++ src/SJSU-Connect-Server/routes/user.js
@@ -1,3 +1,6 @@
+var mongo = require('./mongo');
+var ObjectId = require('mongodb').ObjectID;
+var url = "mongodb://localhost:27017/Facebook";
 
 exports.login=function(req,res){
 	console.log("Reporting from login function");
@@ -5,7 +8,27 @@ exports.login=function(req,res){
 };
 
 exports.register=function(req,res){
-	console.log("Reporting from register new user function");
+    console.log("Reporting from register new user function");
+
+
+
+    mongo.connect(url, function() {
+
+        console.log('CONNECTED TO MONGO IN handle_get_property_list_request');
+        var Users = mongo.collection('Users');
+        var json_response= {};
+
+        Users.find({ email: 'j.n@gmail.com'}).toArray(
+
+            function(err, user) {
+                json_responses = {
+
+                    "Result" : user
+                };
+                console.log(user);
+                res.send({statusCode:200});
+            });
+    });
 };
 
 exports.forgotPassword=function(req,res){
