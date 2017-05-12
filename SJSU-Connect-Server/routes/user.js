bd61caa39e6e1678ec99772a9d05e72009445835
Sagar Mane <sagar.mane006@gmail.com>
2017-05-11 17:02:12 -0700

    Setting up Node Express server for SJSU connect

---
+++ SJSU-Connect-Server/routes/user.js
@@ -0,0 +1,17 @@
+
+exports.login=function(req,res){
+	console.log("Reporting from login function");
+	
+};
+
+exports.register=function(req,res){
+	console.log("Reporting from register new user function");
+};
+
+exports.forgotPassword=function(req,res){
+	console.log("Reporting from forgot password function");
+};
+
+exports.editProfile=function(req,res){
+	console.log("Reporting from editProfile function");
+};
