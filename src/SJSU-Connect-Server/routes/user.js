80c1d9968361c285d4a2b07bffde3b2985c38f08
jnirgudkar1 <jnirgudkar1@gmail.com>
2017-05-12 16:39:11 -0700

    Register user
    Added user to mongo db.

--- src/SJSU-Connect-Server/routes/user.js
+++ src/SJSU-Connect-Server/routes/user.js
@@ -1,6 +1,7 @@
 var mongo = require('./mongo');
 var ObjectId = require('mongodb').ObjectID;
 var url = "mongodb://localhost:27017/Facebook";
+var bcrypt = require('bcrypt-nodejs');
 
 exports.login=function(req,res){
 	console.log("Reporting from login function");
@@ -11,6 +12,13 @@ exports.register=function(req,res){
     console.log("Reporting from register new user function");
 
 
+    var first_name = req.param("first_name");
+    var last_name = req.param("last_name");
+    var country_code = req.param("country_code");
+    var phone_number = req.param("phone_number");
+    var email = req.param("email");
+    var password = req.param("password");
+    var crp = bcrypt.hashSync(password);
 
     mongo.connect(url, function() {
 
@@ -18,16 +26,30 @@ exports.register=function(req,res){
         var Users = mongo.collection('Users');
         var json_response= {};
 
-        Users.find({ email: 'j.n@gmail.com'}).toArray(
+        var user1 = {
+            first_name:	first_name,
+            last_name:	last_name,
+            country_code : country_code,
+            phone_number: phone_number,
+            email : email,
+            password : crp
+        };
+        Users.insert([user1], function (err, result) {
+            if(err)
+            {
+                throw err;
+                db.close();
+            }
 
-            function(err, user) {
-                json_responses = {
+            else if(result.length)
+            {
+                console.log("found user." + result[0]);
+                db.close();
 
-                    "Result" : user
-                };
-                console.log(user);
-                res.send({statusCode:200});
-            });
+                response={"statusCode" : 401};
+                res.send(response);
+            }
+        });
     });
 };
 
