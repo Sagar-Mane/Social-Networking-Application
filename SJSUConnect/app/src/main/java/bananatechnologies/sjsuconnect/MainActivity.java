93c29bd2d9c756f5ace1a457c99b7fca54378713
Sagar Mane <sagar.mane006@gmail.com>
2017-05-11 21:16:28 -0700

    Adding UI for register user activity

--- SJSUConnect/app/src/main/java/bananatechnologies/sjsuconnect/MainActivity.java
+++ SJSUConnect/app/src/main/java/bananatechnologies/sjsuconnect/MainActivity.java
@@ -1,7 +1,6 @@
 package bananatechnologies.sjsuconnect;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
